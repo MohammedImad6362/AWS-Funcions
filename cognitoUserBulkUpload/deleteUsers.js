d4d44a5c564e3e9f872b582027f5f8ff4347f2e1
MohammedImad6362 <mdimad@futuretechschool.in>
2023-11-21 15:51:28 +0530

    deleted dummy users

--- cognitoUserBulkUpload/deleteUsers.js
+++ cognitoUserBulkUpload/deleteUsers.js
@@ -1,23 +1,40 @@
 const AWS = require("aws-sdk");
 
-// Set your AWS region and Cognito User Pool ID
+// Set your AWS region, Cognito User Pool ID, and DynamoDB table name
 AWS.config.update({ region: "ap-south-1" });
 const userPoolId = "ap-south-1_1M7FLYgyw";
+const dynamoDBTableName = "Users"; // Replace with your DynamoDB table name
 
-// Initialize the Cognito Identity Provider client
+// Initialize the Cognito Identity Provider client and DynamoDB Document Client
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 // List of usernames to delete
 const usernamesToDelete = []
+
 async function deleteUser(username) {
   try {
+    // Delete user from Cognito User Pool
     await cognitoIdentityServiceProvider
       .adminDeleteUser({
         UserPoolId: userPoolId,
         Username: username,
       })
       .promise();
-    console.log(`User ${username} deleted successfully.`);
+
+    console.log(`User ${username} deleted from Cognito User Pool successfully.`);
+
+    // Delete user from DynamoDB table
+    await dynamoDB
+      .delete({
+        TableName: dynamoDBTableName,
+        Key: {
+          user_id: username,
+        },
+      })
+      .promise();
+
+    console.log(`User ${username} deleted from DynamoDB table successfully.`);
   } catch (error) {
     if (error.code === "UserNotFoundException") {
       console.log(`User ${username} not found.`);
--- userBulkUpl-Test/index.js
+++ userBulkUpl-Test/index.js
@@ -1,58 +1,75 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const docClient = new AWS.DynamoDB.DocumentClient();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const multipart = require('lambda-multipart-parser');
+const lambda = new AWS.Lambda();
+const uuid = require("uuid");
+const Joi = require("joi");
+
 
 exports.handler = async (event, context) => {
     try {
-        const file = await multipart.parse(event, true);
+        const bulkUploadUsersDetails = Joi.object({
+            instituteId: Joi.string().required(),
+            branchId: Joi.string().required(),
+            batchId: Joi.string().allow(''),
+        });
 
-        if (!file.files || file.files.length === 0) {
+        const formData = await multipart.parse(event, true);
+        if (!formData.files || formData.files.length === 0) {
             return {
                 statusCode: 400,
                 body: JSON.stringify({ message: "No file uploaded" }),
             };
         }
 
-        const uploadedFile = file.files[0];
-        console.log(uploadedFile);
-
-        const file_name = uploadedFile.filename;
-        const { filename, content } = uploadedFile;
-
-        if (!file_name.toLowerCase().endsWith(".xlsx") && !file_name.toLowerCase().endsWith(".xlxs")) {
-            return {
+        const uploadedFile = formData.files[0];
+        if (!filename.toLowerCase().endsWith(".xlsx") && !filename.toLowerCase().endsWith(".xlxs")) {
+            return response({
                 statusCode: 400,
                 body: JSON.stringify({
-                    message: "Uploaded file must be an xlsx or xlxs file only",
+                    message: "Uploaded file must be a xlsx or xlxs file only",
                 }),
-            };
+            });
         }
 
-        const created_at = new Date().toISOString();
-        const file_id = `${file_name} ${created_at}`;
+        const { filename, content } = uploadedFile;
+        const { instituteId, branchId, batchId } = formData;
 
-        // Define your S3 bucket name
+        const { error } = bulkUploadUsersDetails.validate({
+            instituteId,
+            branchId,
+            batchId
+        });
+        if (error) {
+            console.error("Validation error:", error.details[0].message);
+            return response({
+                statusCode: 400,
+                body: JSON.stringify({ message: error.details[0].message }),
+            });
+        }
+
+
+        const processing_id = uuid.v4();
         const bucketName = 'allassestsupmyranks';
-
-        // Upload the Excel file to S3
-        let objectKey = `xlxs/${file_name}`;
+        let objectKey = `xlxs/${filename}`;
 
         await uploadFileToS3(bucketName, objectKey, content);
 
         // Store file information in DynamoDB
-        await storeFileInfoInDynamoDB(file_id, created_at, file_name);
-
-        return {
+        await storeFileInfoInDynamoDB(processing_id, filename, objectKey, instituteId, branchId, batchId);
+        console.log("response: File information stored in DynamoDB and file uploaded to S3 successfully.")
+        return response({
             statusCode: 200,
-            body: JSON.stringify({ file_name, msg: "File information stored in DynamoDB and file uploaded to S3 successfully." }),
-        };
-    } catch (error) {
+            body: JSON.stringify({ processing_id, message: "File processing" }),
+        });
+    }
+    catch (error) {
         console.error(`Error handling multipart/form-data: ${error}`);
-        return {
+        return response({
             statusCode: 500,
-            body: JSON.stringify({ error: error.message })
-        };
+            body: JSON.stringify({ message: error.message })
+        })
     }
 };
 
@@ -61,31 +78,52 @@ async function uploadFileToS3(bucketName, objectKey, content) {
         Bucket: bucketName,
         Key: objectKey,
         Body: content,
-        ContentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        ContentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     };
 
     try {
         await s3.putObject(params).promise();
-    } catch (error) {
+    }
+    catch (error) {
         console.error(`Error uploading file to S3: ${error}`);
         throw error;
     }
 }
 
-async function storeFileInfoInDynamoDB(file_id, created_at, file_name) {
-    const params = {
-        TableName: 'Bulk_Upload',
-        Item: {
-            file_id,
-            created_at,
-            file_name,
-        },
-    };
-
-    try {
-        await docClient.put(params).promise();
-    } catch (error) {
-        console.error(`Error storing file information in DynamoDB: ${error}`);
-        throw error;
+async function storeFileInfoInDynamoDB(processing_id, filename, objectKey, instituteId, branchId, batchId) {
+    const dbItem = {
+        processing_id,
+        created_at: new Date().toISOString(),
+        filename,
+        s3Path: objectKey,
+        uploaded: false,
+        instituteId,
+        branchId,
+        batchId: batchId || null,
+        branchBatchId: batchId ? `${branchId}_${batchId}` : branchId 
     }
+    const params = {
+        TableName: 'BulkUploadFiles',
+        Item: dbItem,
+    };
+    await dynamoDB.put(params).promise();
+    const paramsToLambda = {
+        FunctionName: "Create_Cognito_users",
+        InvocationType: "Event",
+        Payload: JSON.stringify(dbItem),
+    };
+    await lambda.invoke(paramsToLambda).promise();
 }
+
+const response = (res) => {
+    return {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+        statusCode: res.statusCode,
+        body: res.body,
+    };
+};
